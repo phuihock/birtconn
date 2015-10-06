840f6fe0d3be774c46ab535240ee851cb013e35c
Chang Phui-Hock <phuihock@codekaki.com>
2015-10-06 23:52:46 +0800

    [ADD] logging with log4j2

--- src/main/java/com/codekaki/odoo/birtrpt/Application.java
+++ src/main/java/com/codekaki/odoo/birtrpt/Application.java
@@ -1,8 +1,5 @@
 package com.codekaki.odoo.birtrpt;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javax.json.stream.JsonGenerator;
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -10,28 +7,41 @@ import javax.naming.NameClassPair;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.birt.report.engine.api.ReportRunner;
 import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class Application extends ResourceConfig {
-    static protected Logger logger = Logger.getLogger(ReportRunner.class.getName());
-    
+    static final Logger logger = LogManager.getLogger(ReportRunner.class);
+
     public Application() {
         packages("com.codekaki.odoo.birtrpt.resource");
         register(LoggingFilter.class);
         property(JsonGenerator.PRETTY_PRINTING, true);
+        logger.info("Listing initial context java:comp/env");
+        Context ctx;
         try {
-            logger.log(Level.INFO, "Listing initial context java:comp/env");
-            Context ctx = new InitialContext();
-            NamingEnumeration<NameClassPair> list = ctx.list("java:comp/env");
-            while(list.hasMore()){
-                NameClassPair pair = list.next();
-                logger.log(Level.INFO, "- " + pair.getName());
-            }
-            System.out.println("No more entries!");
+            ctx = new InitialContext();
+            walk(ctx, "env");
         } catch (NamingException e) {
-            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+
+    private void walk(Context ctx, String name) {
+        NamingEnumeration<NameClassPair> list = null;
+        try {
+            list = ctx.list("java:comp/" + name);
+            while (list.hasMore()) {
+                NameClassPair pair = list.next();
+                if(!pair.getName().equals("__")){
+                    String cname = name + "/" + pair.getName();
+                    walk(ctx, cname);
+                }
+            }
+        } catch (NamingException e) {
+            logger.info("java:comp/" + name);
         }
     }
 }
