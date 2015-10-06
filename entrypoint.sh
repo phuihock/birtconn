49c693f2fd8f9fb3ecc36a89b813548b2dfc4061
Chang Phui-Hock <phuihock@codekaki.com>
2015-10-06 23:52:13 +0800

    [IMP] if entrypoint.sh is invoked with a properties file, export all vars into the environment.
    It simulates Docker's --env-file behavior locally for dev purpose.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
-java \
-    -DJDBC_NAME="${JDBC_NAME}" \
-    -DDB_NAME="${DB_NAME}" \
-    -DDB_USER="${DB_USER}" \
-    -DDB_PASS="${DB_PASS}" \
-    -DDB_HOST="${DB_HOST}" \
-    -DDB_PORT="${DB_PORT}" \
-    -DBIRT_RESOURCES="${BIRT_RESOURCES}" \
-    -DBIRT_REPORTS="${BIRT_REPORTS}" \
-    -DBIRT_OUTPUT="${BIRT_OUTPUT}" \
-    -jar /opt/${JETTY_VER}/start.jar \
+#!/bin/bash
+if test -f "$1"; then
+    for var in $(cat "$1"); do
+        export ${var};
+    done;
+fi
+
+exec java -jar ${JETTY_START_JAR} \
+    -Dbirt.resources="${BIRT_RESOURCES}" \
+    -Dbirt.reports="${BIRT_REPORTS}" \
+    -Dbirt.output="${BIRT_OUTPUT}" \
+    -Ddb.host="${DB_HOST}" \
+    -Ddb.port="${DB_PORT}" \
+    -Ddb.name="${DB_NAME}" \
+    -Ddb.user="${DB_USER}" \
+    -Ddb.pass="${DB_PASS}" \
+    --exec \
     jetty.base=jetty
